3dd8dce7dd1c47a5e677e8c31af4dc9895c89b02
Michael Kirby <kirbyma729@gmail.com>
2017-10-03 22:11:12 -0400

    update freq analysis

--- freq_analysis_Caesar.py
+++ freq_analysis_Caesar.py
@@ -5,6 +5,8 @@
 # Monogram and digram frequencies given by the following website
 # http://practicalcryptography.com/cryptanalysis/letter-frequencies-various-languages/english-letter-frequencies/
 
+import sys
+
 monogram_freq = {
 "A" :  8.55,        "K" :  0.81,        "U" :  2.68,
 "B" :  1.60,        "L" :  4.21,        "V" :  1.06,
@@ -17,39 +19,47 @@ monogram_freq = {
 "I" :  7.33,        "S" :  6.73,
 "J" :  0.22,        "T" :  8.94}  #values are in percents
 
-digram_freq = {
-"TH" :  2.71,        "EN" :  1.13,        "NG" :  0.89,
-"HE" :  2.33,        "AT" :  1.12,        "AL" :  0.88,
-"IN" :  2.03,        "ED" :  1.08,        "IT" :  0.88,
-"ER" :  1.78,        "ND" :  1.07,        "AS" :  0.87,
-"AN" :  1.61,        "TO" :  1.07,        "IS" :  0.86,
-"RE" :  1.41,        "OR" :  1.06,        "HA" :  0.83,
-"ES" :  1.32,        "EA" :  1.00,        "ET" :  0.76,
-"ON" :  1.32,        "TI" :  0.99,        "SE" :  0.73,
-"ST" :  1.25,        "AR" :  0.98,        "OU" :  0.72,
-"NT" :  1.17,        "TE" :  0.98,        "OF" :  0.71}  #top 30 digrams, values are in percents
+#convert monogram frequencies from percents to decimal
+#for key in monogram_freq.keys():
+    #monogram_freq[key] = round(monogram_freq[key] / 100, 5)
 
-for key in monogram_freq.keys():
-    monogram_freq[key] = round(monogram_freq[key] / 100, 5)
-#print monogram_freq
-for key in digram_freq.keys():
-    digram_freq[key] = round(digram_freq[key] / 100, 5)
+bigramfile = open("bigram-data.txt", 'r')
+lines = bigramfile.readlines()
+bigramfile.close()
+value = []
+count = []
+total = 0
+for l in xrange(len(lines)):
+    value.append(lines[l][:2])
+    occurances = int(lines[l][2:].strip())
+    count.append(occurances)
+    total += occurances
 
-import sys
+bigram_count = {}
+for k in xrange(len(value)):
+    bigram_count[value[k]] = count[k]
 
+bigram_freq = {}
+for key in bigram_count.keys():
+    bigram_freq[key] = round(bigram_count[key] / float(total) * 100, 5)
+
+#verify correct number of arguments given on the command line
 if (len(sys.argv) < 2):
     sys.exit("Too few arguments. python <script> <text.txt>")
 elif (len(sys.argv) > 2):
     sys.exit("Too many arguments. python <script> <text.txt>")
 
+#open the file and read the text
 textfile = open(sys.argv[1], 'r')
 text = textfile.read()
 textfile.close()
 
+#remove invalid characters and convert all characters to uppercase
 invalid_chars = ' \n,./<>?;:\'"`[]{}\|-=_+!@#$%^&*()1234567890'
 text = text.translate(None, invalid_chars)
 text = text.upper()
 
+#count the number of occurances of each unique letter
 letter_count = {}
 for i in xrange(len(text)):
     if text[i] in letter_count.keys():
@@ -57,10 +67,12 @@ for i in xrange(len(text)):
     elif text[i] not in letter_count.keys():
         letter_count[text[i]] = 1
 
+#calculate the frequency of the letters
 letter_freq = {}
 for key in letter_count.keys():
-    letter_freq[key] = round(float(letter_count[key]) / len(text), 5)
+    letter_freq[key] = round(float(letter_count[key]) / len(text) * 100, 5)
 
+#count the number of occurances of digrams in the text
 digram_count = {}
 n = 0
 while n < len(text):
@@ -70,21 +82,28 @@ while n < len(text):
         digram_count[text[n:n+2]] = 1
     n += 2
 
-print digram_count
-#need a variable name for digram freq
+#calculate the frequency of the digrams
+digram_freq = {}
+for key in digram_count.keys():
+    digram_freq[key] = round(float(digram_count[key]) / len(text) / 2 * 100, 5)
 
+##########################################################################
+#Find the mean squared error of a set of data
+#actual: dictionary object containing the theoretical data value
+#observed: dictionary object containing the experimental data value
 def meanSquaredError(actual, observed):
     accumulator = 0
-    for key in actual:
+    for key in observed.keys():
         tmp = pow(float(actual[key] - observed[key]), 2)
         accumulator += tmp
 
-    return float(accumulator) / len(actual.keys())
+    return float(accumulator) / len(observed.keys())
+##########################################################################
+
+print len(bigram_freq)
+print len(digram_freq)
 
 monogram_error = round(meanSquaredError(monogram_freq, letter_freq), 5)
-
-#print monogram_error
-#print text[:50]
-#print text[-50:]
-#print letter_count
-#print letter_freq
+digram_error = round(meanSquaredError(bigram_freq, digram_freq), 5)
+print "Monogram Error:", monogram_error
+print "Digram Error:", digram_error
