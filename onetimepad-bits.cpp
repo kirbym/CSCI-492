55c7216767e2718e8a96a63a0d440de6e9ba7c50
Michael Kirby <kirbyma729@gmail.com>
2017-10-25 20:15:45 -0400

    en/decrypt any file type

--- onetimepad-bits.cpp
+++ onetimepad-bits.cpp
@@ -8,10 +8,10 @@ int main(int argc, char* argv[]){
     fstream file3;
     file1.open(argv[1], fstream::in | fstream::binary);
     file2.open(argv[2], fstream::in | fstream::binary);
-    file3.open("secretmsg.txt", fstream::out | fstream::trunc | fstream::binary);
+    file3.open(argv[3], fstream::out | fstream::trunc | fstream::binary);
 
-    if(argc != 3){
-        cout << "usage: " << argv[0] << "<text.txt> <key.txt>\n";
+    if(argc != 4){
+        cout << "usage: " << argv[0] << " <input file> <key file> <output file>\n";
     } else {
         if(!file1.is_open() || !file2.is_open() || !file3.is_open()){
             cout << "Could not open one or more of the files\n";
@@ -32,14 +32,13 @@ int main(int argc, char* argv[]){
             for(int i = 0; i < length1; i++){
                 a = buffer1[i];
                 b = buffer2[i % length2];
-                c = ((a ^ b) % 94) + 32;
+                c = (a ^ b);
 
-                if (i % 100 == 0){
-                    file3 << c << '\n';
-                } else {
-                    file3 << c;
-                }
+                file3 << c;
             }
+
+            delete[] buffer1;
+            delete[] buffer2;
         }
     }
 
