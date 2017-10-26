f6243c89f1c2094cb7a3004237b876e6a5782cd4
Michael Kirby <kirbyma729@gmail.com>
2017-10-25 20:45:31 -0400

    add comments

--- onepad.py
+++ onepad.py
@@ -7,20 +7,23 @@ import sys
 def encrypt(ptext, key):
     invalid_chars = ' \n,./<>?;:\'"[]{}\|-=_+!@#$%^&*()1234567890'
     ptext = ptext.translate(None, invalid_chars)  #remove invalid characters from string
-    ptext = ptext.upper()
-    key = key.translate(None, invalid_chars)
+    ptext = ptext.upper()   #convert to uppercase characters
+    key = key.translate(None, invalid_chars)   #same as ptext
     key = key.upper()
 
-    ptext = map(lambda x: ord(x) - ord('A'), ptext)
+    ptext = map(lambda x: ord(x) - ord('A'), ptext)  #convert ASCII values to values mod 26
     key = map(lambda x: ord(x) - ord('A'), key)
 
     n = 0
     ctext = []
+    #implement encryption: shift (add) each letter of plaintext with the value designated by the key,
+    #repeating the key if necessary
+    #poly-alphabetic shift
     while n < len(ptext):
         ctext.append((ptext[n] + key[n % len(key)]) % 26)
         n += 1
 
-    ctext = map(lambda x: chr(x + ord('A')), ctext)
+    ctext = map(lambda x: chr(x + ord('A')), ctext)   #convert mod 26 representation back to ASCII representation
     ctext = "".join(ctext)
 
     #print "plaintext", ptext
@@ -32,20 +35,23 @@ def encrypt(ptext, key):
 def decrypt(ctext, key):
     invalid_chars = ' \n,./<>?;:\'"[]{}\|-=_+!@#$%^&*()1234567890'
     ctext = ctext.translate(None, invalid_chars)  #remove invalid characters from string
-    ctext = ctext.upper()
-    key = key.translate(None, invalid_chars)
+    ctext = ctext.upper()     #convert to uppercase characters
+    key = key.translate(None, invalid_chars)    #same as ctext
     key = key.upper()
 
-    ctext = map(lambda x: ord(x) - ord('A'), ctext)
+    ctext = map(lambda x: ord(x) - ord('A'), ctext)   #convert ASCII values to values mod 26
     key = map(lambda x: ord(x) - ord('A'), key)
 
     n = 0
     ptext = []
+    #implement decryption: shift (subtract) each letter of plaintext with the value designated by the key,
+    #repeating the key if necessary
+    #poly-alphabetic shift
     while n < len(ctext):
         ptext.append((ctext[n] - key[n % len(key)]) % 26)
         n += 1
 
-    ptext = map(lambda x: chr(x + ord('A')), ptext)
+    ptext = map(lambda x: chr(x + ord('A')), ptext)  #convert mod 26 representation back to ASCII representation
     ptext = "".join(ptext)
 
     #print "ciphertext", ctext
--- onetimepad-bits.cpp
+++ onetimepad-bits.cpp
@@ -1,28 +1,34 @@
+//Michael Kirby
+//10/25/17
+//One time pad encryption and decryption scheme implemented in C++
+//Able to encrypt files of any type (encrypt .jpeg with another .jpeg)
+
 #include <iostream>
-#include <fstream>
+#include <fstream>    //file processing
 using namespace std;
 
+//take command line arguments, argc: number of arguments including exe file, argv[]: array of given arguments
 int main(int argc, char* argv[]){
-    fstream file1;
+    fstream file1;  //file objects
     fstream file2;
     fstream file3;
-    file1.open(argv[1], fstream::in | fstream::binary);
+    file1.open(argv[1], fstream::in | fstream::binary);  //open 2 files to read from in binary mode
     file2.open(argv[2], fstream::in | fstream::binary);
-    file3.open(argv[3], fstream::out | fstream::trunc | fstream::binary);
+    file3.open(argv[3], fstream::out | fstream::trunc | fstream::binary);   //open file to write to in binary mode
 
-    if(argc != 4){
+    if(argc != 4){  //check number of command line arguments
         cout << "usage: " << argv[0] << " <input file> <key file> <output file>\n";
     } else {
-        if(!file1.is_open() || !file2.is_open() || !file3.is_open()){
+        if(!file1.is_open() || !file2.is_open() || !file3.is_open()){   //check if problems opening files
             cout << "Could not open one or more of the files\n";
         } else {
-            file1.seekg(0, file1.end);
+            file1.seekg(0, file1.end);    //move file pointer to end of file to find length of file (with tellg())
             int length1 = file1.tellg();
-            file1.seekg(0, file1.beg);
-            char* buffer1 = new char [length1];
-            file1.read(buffer1, length1);
+            file1.seekg(0, file1.beg);      //move file pointer to beginning
+            char* buffer1 = new char [length1];     //character array for all bytes of file
+            file1.read(buffer1, length1);       //read whole file
 
-            file2.seekg(0, file2.end);
+            file2.seekg(0, file2.end);      //same process as file1
             int length2 = file2.tellg();
             file2.seekg(0, file2.beg);
             char* buffer2 = new char [length2];
@@ -30,19 +36,19 @@ int main(int argc, char* argv[]){
 
             char a, b, c;
             for(int i = 0; i < length1; i++){
-                a = buffer1[i];
-                b = buffer2[i % length2];
-                c = (a ^ b);
-
-                file3 << c;
+                a = buffer1[i];     //get each element of input file
+                b = buffer2[i % length2];  //get elements of key file (repeat if necessary)
+                c = (a ^ b);        //XOR bytes to encrypt input file with key file
+                //same process works in reverse to decrypt a file (XOR output with key to return to input)
+                file3 << c;   //write to output file
             }
 
-            delete[] buffer1;
+            delete[] buffer1;   //delete arrays to deallocate memory
             delete[] buffer2;
         }
     }
 
-    file1.close();
+    file1.close();    //close all files
     file2.close();
     file3.close();
     return 0;
