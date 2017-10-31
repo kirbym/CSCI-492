4ccb4685e23152680c43e0ce1b8b4ed761164958
Michael Kirby <kirbyma729@gmail.com>
2017-10-31 19:37:22 -0400

    improved functionality

--- rsa-smallprime.py
+++ rsa-smallprime.py
@@ -2,24 +2,26 @@
 # Michael Kirby
 # 10/29/17
 
-#input string (as numbers) must be less than n?
+#input string (as numbers) must be less than n? yes, otherwise must chunk msg
 #if input is relatively prime to n, msg gets encrypted to itself?
 
 import sys
 import fractions
 
-def checkPrimality(num):
-    isPrime = True
-    if num % 2 == 0:
+def isPrime(num):   #checking if a number is prime
+    valid = True
+    if num <= 1:
+        return False
+    elif num % 2 == 0:
         return False
     else:
-        for i in xrange(3, num, 2):
+        for i in range(3, num, 2):
             if i*i <= num:
                 if num % i == 0:
-                    isPrime = False
+                    valid = False
                     break
 
-        return isPrime
+        return valid
 
 def encrypt(n, e, m):    #step 5
     return pow(m, e) % n
@@ -28,41 +30,51 @@ def decrypt(n, d, c):    #step 6
     return pow(c, d) % n
 
 if __name__ == "__main__":
-    #textfile = open(sys.argv[1], 'r')
-    #text = textfile.read()
-    #textfile.close()
-    m = int(sys.argv[1])
-    p = int(sys.argv[2])
-    q = int(sys.argv[3])
+    # get input values
+    m = int(sys.argv[1])  #message entered as number
+    p = int(sys.argv[2])  #prime #1  =/= q
+    q = int(sys.argv[3])  #prime #2  =/= p
 
-    if checkPrimality(p) and checkPrimality(q):
-        n = p*q  #step 1
+    if p == q:
+        sys.exit("Must enter two distinct prime numbers")
+    else:
+        if not isPrime(p) or not isPrime(q):
+            print "One or both of the numbers are not prime"
+            if not isPrime(p):
+                print "%d is not prime" % (p)
+            if not isPrime(q):
+                print "%d is not prime" % (q)
+            sys.exit(1)
+        else:
+            n = p*q  #step 1
+            if m > n:
+                print "Message is too large, must be less than %d*%d = %d" % (p, q, n)
+                sys.exit(1)
+            else:
+                prod = (p-1)*(q-1)
+                x = prod + 2*p + 2*q    #starting value when searching for 'e'
+                e = None            #encryption exponent
+                while e is None:
+                    x += 1
+                    if fractions.gcd(x, prod) == 1:     #'e' and (p-1)(q-1) must be relatively prime
+                        e = x        #step 2
 
-        pq = (p-1)*(q-1)
-        x = pq + 2*p - 2*q
-        e = None
-        while e is None:
-            x += 1
-            if fractions.gcd(x, pq) == 1:
-                e = x        #step 2
-
-        d = None
-        y = pq + 2*q - 2*p
-        while d is None:
-            y += 1
-            #print "y:", y, "ye:", y*e, "ye mod pq:", (y*e) % pq
-            if (y*e) % pq == 1:
-                d = y     #step 3
+                y = prod + 2*p + 2*q   #starting value when searching for 'd'
+                d = None     #decryption exponent
+                while d is None:
+                    y += 1
+                    if (y*e) % prod == 1:       #'de' congruent to 1 mod (p-1)(q-1)
+                        d = y     #step 3
 
 
-        c = encrypt(n, e, m)
-        z = decrypt(n, d, c)
-        print "input:", m
-        print "p:", p
-        print "q:", q
-        print "n:", n
-        print "pq:", pq
-        print "e:", e
-        print "d:", d
-        print "encrypted:", c
-        print "decrypted:", z
+                c = encrypt(n, e, m)
+                z = decrypt(n, d, c)
+                print "input:", m
+                print "p:", p
+                print "q:", q
+                print "n:", n
+                print "(p-1)(q-1):", prod
+                print "e:", e
+                print "d:", d
+                print "encrypted:", c
+                print "decrypted:", z
